f4f39337a66816f3250b2653dcb2d24d18b00378
Andre Wells <awells@bscglobal.com>
2023-02-15 11:34:48 +0200

    Added no unused imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,20 +24,22 @@ module.exports = {
   },
   plugins: [
     'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
+    'unused-imports'
   ],
   overrides: [
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_"
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
       }
-    ],    
+    ],
+    'unused-imports/no-unused-imports': 'error',
   }
 }
